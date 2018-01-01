002a18d0f8d9c058d3394a143b6d4665fb6568c8
Tuncay Cansız <32176945+Devinsomnia@users.noreply.github.com>
2018-01-01 17:53:47 +0300

    Initial Commit

---
+++ LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Tuncay Cansız on 1.01.2018.
+//  Copyright © 2018 Tuncay Cansız. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
